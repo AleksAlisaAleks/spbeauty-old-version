1abb15af97a86c3bf7611cf8fa921cb4cc06c8fb
AleksAlisaAleks <130169554+AleksAlisaAleks@users.noreply.github.com>
2023-04-17 12:58:42 +0000

    addresses updated

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -1,17 +1,38 @@
 import PrimaryButton from "../../../../../Components/PrimaryButton";
-import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
+
+const title = {
+  fontFamily: 'Inter',
+  fontSize: '20px',
+  fontWeight: '600',
+  lineHeight: '24px',
+  letterSpacing: '0em',
+  textAlign: 'left',
+  marginLeft: '9px',
+};
+
+const containerStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  height: '100%',
+  border: '1px solid #E2E8F0',
+  borderRadius: '4px',
+  padding: '20px 16px',
+  marginRight: '100px',
+};
+
 function Addresses() {
-    return (
-        <div className='d-inline-flex flex-column gap-2 mt-4'>
-            <div className='text-rg'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-3'>
-                <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
-                <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                <PrimaryButton>Добавить филиал</PrimaryButton>
-            </div>
-        </div>
-    );
+  return (
+    <div style={containerStyle}>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+        <div style={title}>Адреса заведений</div>
+        <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
+        <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
+      </div>
+      <PrimaryButton style={{ alignSelf: 'flex-end' }}>Добавить филиал</PrimaryButton>
+    </div>
+  );
 }
 
 export default Addresses;
