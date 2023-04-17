10be25c26d37d24d5880917ca1b05346164c5f71
AleksAlisaAleks <130169554+AleksAlisaAleks@users.noreply.github.com>
2023-04-17 10:47:29 +0000

    TitledSelector done

--- src/Components/TitledSelector.js
+++ src/Components/TitledSelector.js
@@ -7,19 +7,35 @@ const Select = styled.select`
     -webkit-appearance: none !important; 
     appearance: none !important;
     outline: none;
-    border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
+    flex-direction: row;
+    align-items: center;
+    padding: 8px 16px;
+    gap: 16px;
+    border-radius: 24px;
+    border: 1px solid #E2E8F0;
+    background-color: #F8FAFC;
     color: #666787;
+    width: 447px;
+    height: 36px;
     &:hover {
-        background-color: #D9E2EFB8;
+        background-color: #0F172A;
     }
 `;
 
-function TitledSelector({title, options}) {
-    const Title = title === undefined ? 
-        (<div></div>) : 
-        (<div>{title}</div>);
+const titleStyle = {
+    fontFamily: 'Inter',
+    fontStyle: 'normal',
+    fontWeight: 400,
+    fontSize: '0.75em',
+    lineHeight: 1,
+    color: '#94A3B8',
+    marginBottom: '0.5em'
+  };
+
+function TitledSelector({ title, options }) {
+    const Title = title === undefined ?
+        (<div></div>) :
+        (<div style={titleStyle}>{title}</div>);
     return (
         <div className='d-flex flex-column gap-2'>
             {Title}
