09733551481df2629b554e73b40c5f5733136a30
AleksAlisaAleks <130169554+AleksAlisaAleks@users.noreply.github.com>
2023-04-18 16:46:29 +0000

    primarybutton color changed

--- src/Components/PrimaryButton.js
+++ src/Components/PrimaryButton.js
@@ -7,12 +7,12 @@ function PrimaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
     let minWidth = props.minWidth === undefined ? '220px' : props.minWidth; 
     const Button = styled.div`
-        background: #FFFFFF;
+        background: #3B82F6;
         border: 1px solid #152842;
         border-radius: 24px;
         box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
         min-width: ${minWidth};
-        color:  #64748B;
+        color:  #FFFFFF;
         &:hover {
             background: #3B82F6;
             border-color: transparent;
